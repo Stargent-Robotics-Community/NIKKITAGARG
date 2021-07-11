3d0c28d8981a1a831d084e8514c01f767f097386
NIKKITAGARG <nikkigarg306@gmail.com>
2021-07-11 07:06:18 +0530

    python assignment done

---
+++ Ques 1-3.py
@@ -0,0 +1,13 @@
+lst = [ 'Money heist', 'Instagram', 'Whatsapp', 'YouTube' ]
+print(list)
+for i in lst:
+    print(lst)
+lst = [ 'Money heist', 'Instagram', 'Whatsapp', 'YouTube' ]
+# Question2
+show1 = input()
+show2 = input()
+lst.append(show1)
+lst.append(show2)
+print(lst)
+# Question 3
+print(lst[1::2])
